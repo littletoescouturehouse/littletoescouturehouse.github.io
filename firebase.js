95b6e380ba908ba971a66f2ce8c2c2d5f4216789
littletoescouturehouse <164710771+littletoescouturehouse@users.noreply.github.com>
2024-03-24 08:32:57 +0000

    furqan

---
+++ firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDtP596I9VQWV4p_g-qObF1w_ijyhZ3AwY",
+    authDomain: "littletoescouturehouse-d7571.firebaseapp.com",
+    projectId: "littletoescouturehouse-d7571",
+    storageBucket: "littletoescouturehouse-d7571.appspot.com",
+    messagingSenderId: "984513161948",
+    appId: "1:984513161948:web:5571dc98cd116dd2b4e219",
+    measurementId: "G-KZ5T1F8BXB"
+    };
+    
+    firebase.initializeApp(firebaseConfig);
