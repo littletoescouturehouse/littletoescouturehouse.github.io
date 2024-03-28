a4152aedbb85f9e859ef4775345dfba42c9a6700
Hamza Durrani <Info@Thecodingimpact.Com>
2024-03-28 17:21:44 +0500

    designing day 3

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ function closeNav() {
     sidePanel.style.left = "-320px";
   }
 }
+
+// --------------------------------------------------slider------------------------------------------
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("menu-image-visa");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
+}
